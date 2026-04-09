6e708dcbd63941f4a94c68b878db1e2e8a59b4f6
leejuhyun1211 <cnrrnrhd6238@gmail.com>
2026-04-09 16:39:57 +0900

    Bump cache to v3 - force refresh

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'baccarat-v1';
-const FILES = ['/', '/Jh/', '/Jh/index.html', '/Jh/manifest.json'];
+const CACHE = 'baccarat-v3';
+const FILES = ['/Jh/', '/Jh/index.html', '/Jh/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES).catch(() => {})));
@@ -8,11 +8,14 @@ self.addEventListener('install', e => {
 
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-    if(res.ok){ const c = res.clone(); caches.open(CACHE).then(cache => cache.put(e.request, c)); }
-    return res;
-  }).catch(() => caches.match('/Jh/index.html'))));
+  e.respondWith(
+    fetch(e.request).then(res => {
+      if(res.ok){ const c=res.clone(); caches.open(CACHE).then(cache=>cache.put(e.request,c)); }
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
 });
